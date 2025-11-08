b434f64ce752bbcf9deb32ebc7b805cab49aef4a
Denis Mokin <denmokn@gmail.com>
2025-11-08 22:39:04 +0200

    Updated Ball Random-color + Random Size

--- main.js
+++ main.js
@@ -7,13 +7,14 @@ var gBallSize = '100px'
 function onBallClick(el, ev) {
 
     if (ev.type === 'click') {
-        gBallSize = parseFloat(gBallSize) + 50
+        gBallSize = parseFloat(gBallSize) + getRandomInt(20, 60)
         if (gBallSize > 400) {
             gBallSize = el.style.width = el.style.height = 100 + 'px'
-            el.innerText = parseFloat(gBallSize) 
+            el.innerText = parseFloat(gBallSize)
             return
         }
 
+        el.style.backgroundColor = randColor() + ''
         el.style.height = el.style.width = gBallSize + 'px'
         el.innerText = gBallSize
     }
